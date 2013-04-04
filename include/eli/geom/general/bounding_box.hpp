f36b30e9747c888bfd63e58b2577ec1bbe3998a8
David D. Marshall <ddmarshall@gmail.com>
2013-04-03 17:00:08 -0700

    Added bounding box class with ability to add points to the bounding box, determine whether point is inside or outside, and whether two bounding boxes intersect.

---
+++ include/eli/geom/general/bounding_box.hpp
@@ -0,0 +1,203 @@
+/*********************************************************************************
+* Copyright (c) 2013 David D. Marshall <ddmarsha@calpoly.edu>
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    David D. Marshall - initial code and implementation
+********************************************************************************/
+
+#ifndef eli_geom_general_bounding_box_hpp
+#define eli_geom_general_bounding_box_hpp
+
+#include "Eigen/Eigen"
+
+#include "eli/util/tolerance.hpp"
+
+namespace eli
+{
+  namespace geom
+  {
+    namespace general
+    {
+      template<typename data__, unsigned short dim__, typename tol__=eli::util::tolerance<data__> >
+      class bounding_box
+      {
+        public:
+          typedef data__ data_type;
+          typedef unsigned short dimension_type;
+          typedef Eigen::Matrix<data_type, 1, dim__> point_type;
+          typedef typename point_type::Index index_type;
+
+        public:
+          bounding_box() : empty(true) {}
+          bounding_box(const point_type &p) : empty(false), pmin(p), pmax(p) {}
+          bounding_box(const bounding_box<data_type, dim__> &bb) : empty(bb.empty), pmin(bb.pmin), pmax(bb.pmax) {}
+          ~bounding_box() {}
+
+          bounding_box<data_type, dim__> & operator=(const bounding_box<data_type, dim__> &bb)
+          {
+            if (this!=&bb)
+            {
+              empty=bb.empty;
+              pmin=bb.pmin;
+              pmax=bb.pmax;
+            }
+
+            return (*this);
+          }
+
+          bool operator==(const bounding_box<data_type, dim__> &bb)
+          {
+            // if compared against self then true
+            if (this==&bb)
+              return true;
+
+            // if both are empty sets then true
+            if (empty && bb.empty)
+              return true;
+
+            // start comparing member data
+            if (empty!=bb.empty)
+              return false;
+            if (pmin!=bb.pmin)
+              return false;
+            if (pmax!=bb.pmax)
+              return false;
+
+            return true;
+          }
+
+          bool operator!=(const bounding_box<data_type, dim__> &bb)
+          {
+            return !operator==(bb);
+          }
+
+          static dimension_type dimension() {return dim__;}
+
+          bool empty_set() const {return empty;}
+
+          void set_min(const point_type &pm)
+          {
+            if (empty_set())
+            {
+              pmax=pm;
+              empty=false;
+            }
+            pmin=pm;
+          }
+          point_type get_min() const {return pmin;}
+
+          void set_max(const point_type &pm)
+          {
+            if (empty_set())
+            {
+              pmin=pm;
+              empty=false;
+            }
+            pmax=pm;
+          }
+          point_type get_max() const {return pmax;}
+
+          bool add(const point_type &p)
+          {
+            index_type i;
+            bool changed(false);
+
+            // handle special case of no bounding box
+            if (empty_set())
+            {
+              set_min(p);
+              set_max(p);
+              return true;
+            }
+
+            for (i=0; i<dim__; ++i)
+            {
+              if (p(i)<pmin(i))
+              {
+                changed=true;
+                pmin(i)=p(i);
+              }
+              if (p(i)>pmax(i))
+              {
+                changed=true;
+                pmax(i)=p(i);
+              }
+            }
+
+            return changed;
+          }
+
+          bool add(const bounding_box<data_type, dim__> &bb)
+          {
+            bool rtn1=add(bb.pmin), rtn2=add(bb.pmax);
+            return (rtn1 || rtn2);
+          }
+
+          bool inside(const point_type &p) const
+          {
+            for (index_type i=0; i<dim__; ++i)
+            {
+              if ( (p(i)<pmin(i)) || (p(i)>pmax(i)) )
+                return false;
+            }
+
+            return true;
+          }
+
+          bool intersect(const bounding_box<data_type, dim__> &bb) const
+          {
+            const index_type nc(1<<dim__);
+            point_type c[nc], bb_min(bb.get_min()), bb_max(bb.get_max());
+
+            // set the first and last corner
+            c[0]=bb_min;
+            c[nc-1]=bb_max;
+
+            // set the corners for 2 or 3 dimensional cases
+            if (dim__>1)
+            {
+              c[1].x()=bb_max.x();
+              c[1].y()=bb_min.y();
+              c[2].x()=bb_min.x();
+              c[2].y()=bb_max.y();
+              if (dim__==3)
+              {
+                c[1].z()=bb_min.z();
+                c[2].z()=bb_min.z();
+              }
+            }
+
+            // set the remaining corners for 3 dimensional cases
+            if (dim__>2)
+            {
+              c[3] << bb_max.x(), bb_max.y(), bb_min.z();
+              c[4] << bb_min.x(), bb_min.y(), bb_max.z();
+              c[5] << bb_max.x(), bb_min.y(), bb_max.z();
+              c[6] << bb_min.x(), bb_max.y(), bb_max.z();
+            }
+
+            // if any corner is inside this bbox, then they intersect
+            for (index_type i=0; i<nc; ++i)
+            {
+              if (inside(c[i]))
+                return true;
+            }
+
+            // at this point know that no bb edge crosses this bbox, but need
+            // to check if this bbox in entirely inside bb
+            return bb.inside(pmin);
+          }
+
+        private:
+          bool empty;
+          point_type pmin, pmax;
+      };
+    }
+  }
+}
+#endif
---
+++ test/geom/bounding_box/bounding_box_test_suite.hpp
@@ -0,0 +1,632 @@
+/*********************************************************************************
+* Copyright (c) 2013 David D. Marshall <ddmarsha@calpoly.edu>
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    David D. Marshall - initial code and implementation
+********************************************************************************/
+
+#ifndef bounding_box_test_suite_hpp
+#define bounding_box_test_suite_hpp
+
+#include <cassert>  // assert()
+#include <cmath>    // cos(), sin()
+
+#include <typeinfo> // typeid
+
+#include "Eigen/Eigen"
+
+#include "eli/code_eli.hpp"
+
+#include "eli/geom/general/bounding_box.hpp"
+
+template<typename data__>
+class bounding_box_test_suite : public Test::Suite
+{
+  private:
+    typedef data__ data_type;
+    typedef eli::geom::general::bounding_box<data__, 1> bounding_box_type1;
+    typedef typename bounding_box_type1::point_type point_type1;
+    typedef eli::geom::general::bounding_box<data__, 2> bounding_box_type2;
+    typedef typename bounding_box_type2::point_type point_type2;
+    typedef eli::geom::general::bounding_box<data__, 3> bounding_box_type3;
+    typedef typename bounding_box_type3::point_type point_type3;
+
+    typedef typename bounding_box_type1::index_type index_type;
+
+  protected:
+    void AddTests(const float &)
+    {
+      // add the tests
+      TEST_ADD(bounding_box_test_suite<float>::construction_test);
+      TEST_ADD(bounding_box_test_suite<float>::add_test);
+      TEST_ADD(bounding_box_test_suite<float>::add_bb_test);
+      TEST_ADD(bounding_box_test_suite<float>::inside_test);
+      TEST_ADD(bounding_box_test_suite<float>::intersect_test);
+    }
+    void AddTests(const double &)
+    {
+      // add the tests
+      TEST_ADD(bounding_box_test_suite<double>::construction_test);
+      TEST_ADD(bounding_box_test_suite<double>::add_test);
+      TEST_ADD(bounding_box_test_suite<double>::add_bb_test);
+      TEST_ADD(bounding_box_test_suite<double>::intersect_test);
+      TEST_ADD(bounding_box_test_suite<double>::inside_test);
+    }
+    void AddTests(const long double &)
+    {
+      // add the tests
+      TEST_ADD(bounding_box_test_suite<long double>::construction_test);
+      TEST_ADD(bounding_box_test_suite<long double>::add_test);
+      TEST_ADD(bounding_box_test_suite<long double>::add_bb_test);
+      TEST_ADD(bounding_box_test_suite<long double>::inside_test);
+      TEST_ADD(bounding_box_test_suite<long double>::intersect_test);
+    }
+#ifdef ELI_QD_FOUND
+    void AddTests(const dd_real &)
+    {
+      // add the tests
+      TEST_ADD(bounding_box_test_suite<dd_real>::construction_test);
+      TEST_ADD(bounding_box_test_suite<dd_real>::add_test);
+      TEST_ADD(bounding_box_test_suite<dd_real>::add_bb_test);
+      TEST_ADD(bounding_box_test_suite<dd_real>::inside_test);
+      TEST_ADD(bounding_box_test_suite<dd_real>::intersect_test);
+    }
+
+    void AddTests(const qd_real &)
+    {
+      // add the tests
+      TEST_ADD(bounding_box_test_suite<qd_real>::construction_test);
+      TEST_ADD(bounding_box_test_suite<qd_real>::add_test);
+      TEST_ADD(bounding_box_test_suite<qd_real>::add_bb_test);
+      TEST_ADD(bounding_box_test_suite<qd_real>::inside_test);
+      TEST_ADD(bounding_box_test_suite<qd_real>::intersect_test);
+    }
+#endif
+  public:
+    bounding_box_test_suite()
+    {
+      AddTests(data__());
+    }
+    ~bounding_box_test_suite()
+    {
+    }
+
+  private:
+    void construction_test()
+    {
+      // test 1D
+      {
+        bounding_box_type1 bb1, bb2;
+
+        TEST_ASSERT(bb1.empty_set());
+
+        bb2=bb1;
+        TEST_ASSERT(bb2==bb1);
+
+        point_type1 pt1;
+        pt1 << 1;
+
+        bounding_box_type1 bb3(pt1);
+
+        TEST_ASSERT(!bb3.empty_set());
+        TEST_ASSERT(bb3.get_min()==pt1);
+        TEST_ASSERT(bb3.get_max()==pt1);
+
+        bounding_box_type1 bb4(bb1);
+
+        TEST_ASSERT(bb1==bb4);
+      }
+
+      // test 2D
+      {
+        bounding_box_type2 bb1, bb2;
+
+        TEST_ASSERT(bb1.empty_set());
+
+        bb2=bb1;
+        TEST_ASSERT(bb2==bb1);
+
+        point_type2 pt1;
+        pt1 << 1, 2;
+
+        bounding_box_type2 bb3(pt1);
+
+        TEST_ASSERT(!bb3.empty_set());
+        TEST_ASSERT(bb3.get_min()==pt1);
+        TEST_ASSERT(bb3.get_max()==pt1);
+
+        bounding_box_type2 bb4(bb1);
+
+        TEST_ASSERT(bb1==bb4);
+      }
+
+      // test 3D
+      {
+        bounding_box_type3 bb1, bb2;
+
+        TEST_ASSERT(bb1.empty_set());
+
+        bb2=bb1;
+        TEST_ASSERT(bb2==bb1);
+
+        point_type3 pt1;
+        pt1 << 1, 2,  3;
+
+        bounding_box_type3 bb3(pt1);
+
+        TEST_ASSERT(!bb3.empty_set());
+        TEST_ASSERT(bb3.get_min()==pt1);
+        TEST_ASSERT(bb3.get_max()==pt1);
+
+        bounding_box_type3 bb4(bb1);
+
+        TEST_ASSERT(bb1==bb4);
+      }
+    }
+
+    void add_test()
+    {
+      // test 1D
+      {
+        bounding_box_type1 bb;
+        point_type1 pt1, pt2, pt3, pt4, pt5;
+        bool changed;
+
+        pt1 << 1;
+        pt2 << 3;
+        pt3 << 4;
+        pt4 << -1;
+        pt5 << 0;
+
+        TEST_ASSERT(bb.empty_set());
+
+        changed=bb.add(pt1);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb.get_min()==pt1);
+        TEST_ASSERT(bb.get_max()==pt1);
+
+        changed=bb.add(pt2);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb.get_min()==pt1);
+        TEST_ASSERT(bb.get_max()==pt2);
+
+        changed=bb.add(pt3);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb.get_min()==pt1);
+        TEST_ASSERT(bb.get_max()==pt3);
+
+        changed=bb.add(pt4);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb.get_min()==pt4);
+        TEST_ASSERT(bb.get_max()==pt3);
+
+        changed=bb.add(pt5);
+        TEST_ASSERT(!changed);
+        TEST_ASSERT(bb.get_min()==pt4);
+        TEST_ASSERT(bb.get_max()==pt3);
+      }
+
+      // test 2D
+      {
+        bounding_box_type2 bb;
+        point_type2 pt1, pt2, pt3, pt4, pt5;
+        bool changed;
+
+        pt1 << 1, 2;
+        pt2 << 3, 4;
+        pt3 << 4, 1;
+        pt4 << -1, 5;
+        pt5 << 0, 3;
+
+        TEST_ASSERT(bb.empty_set());
+
+        changed=bb.add(pt1);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb.get_min()==pt1);
+        TEST_ASSERT(bb.get_max()==pt1);
+
+        changed=bb.add(pt2);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb.get_min()==pt1);
+        TEST_ASSERT(bb.get_max()==pt2);
+
+        changed=bb.add(pt3);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb.get_min().x()==pt1.x());
+        TEST_ASSERT(bb.get_min().y()==pt3.y());
+        TEST_ASSERT(bb.get_max().x()==pt3.x());
+        TEST_ASSERT(bb.get_max().y()==pt2.y());
+
+        changed=bb.add(pt4);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb.get_min().x()==pt4.x());
+        TEST_ASSERT(bb.get_min().y()==pt3.y());
+        TEST_ASSERT(bb.get_max().x()==pt3.x());
+        TEST_ASSERT(bb.get_max().y()==pt4.y());
+
+        changed=bb.add(pt5);
+        TEST_ASSERT(!changed);
+        TEST_ASSERT(bb.get_min().x()==pt4.x());
+        TEST_ASSERT(bb.get_min().y()==pt3.y());
+        TEST_ASSERT(bb.get_max().x()==pt3.x());
+        TEST_ASSERT(bb.get_max().y()==pt4.y());
+      }
+
+      // test 3D
+      {
+        bounding_box_type3 bb;
+        point_type3 pt1, pt2, pt3, pt4, pt5;
+        bool changed;
+
+        pt1 << 1, 2, 1;
+        pt2 << 3, 4, 2;
+        pt3 << 4, 1, -1;
+        pt4 << -1, 5, 4;
+        pt5 << 0, 3, 0;
+
+        TEST_ASSERT(bb.empty_set());
+
+        changed=bb.add(pt1);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb.get_min()==pt1);
+        TEST_ASSERT(bb.get_max()==pt1);
+
+        changed=bb.add(pt2);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb.get_min()==pt1);
+        TEST_ASSERT(bb.get_max()==pt2);
+
+        changed=bb.add(pt3);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb.get_min().x()==pt1.x());
+        TEST_ASSERT(bb.get_min().y()==pt3.y());
+        TEST_ASSERT(bb.get_min().z()==pt3.z());
+        TEST_ASSERT(bb.get_max().x()==pt3.x());
+        TEST_ASSERT(bb.get_max().y()==pt2.y());
+        TEST_ASSERT(bb.get_max().z()==pt2.z());
+
+        changed=bb.add(pt4);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb.get_min().x()==pt4.x());
+        TEST_ASSERT(bb.get_min().y()==pt3.y());
+        TEST_ASSERT(bb.get_min().z()==pt3.z());
+        TEST_ASSERT(bb.get_max().x()==pt3.x());
+        TEST_ASSERT(bb.get_max().y()==pt4.y());
+        TEST_ASSERT(bb.get_max().z()==pt4.z());
+
+        changed=bb.add(pt5);
+        TEST_ASSERT(!changed);
+        TEST_ASSERT(bb.get_min().x()==pt4.x());
+        TEST_ASSERT(bb.get_min().y()==pt3.y());
+        TEST_ASSERT(bb.get_min().z()==pt3.z());
+        TEST_ASSERT(bb.get_max().x()==pt3.x());
+        TEST_ASSERT(bb.get_max().y()==pt4.y());
+        TEST_ASSERT(bb.get_max().z()==pt4.z());
+      }
+    }
+
+    void add_bb_test()
+    {
+      // test 1D
+      {
+        bounding_box_type1 bb1, bb2, bb3;
+        point_type1 pt1, pt2, pt3, pt4, pt5, pt6;
+        bool changed;
+
+        pt1 << 1;
+        pt2 << 3;
+        pt3 << 4;
+        pt4 << -1;
+        pt5 << 0;
+        pt6 << 1;
+
+        bb1.add(pt1);
+        bb1.add(pt2);
+
+        bb2.add(pt3);
+        bb2.add(pt4);
+
+        bb3.add(pt5);
+        bb3.add(pt6);
+
+        // test add that changes bbox
+        changed=bb1.add(bb2);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb1.get_min()==pt4);
+        TEST_ASSERT(bb1.get_max()==pt3);
+
+        // test add that does change bbox
+        changed=bb1.add(bb3);
+        TEST_ASSERT(!changed);
+        TEST_ASSERT(bb1.get_min()==pt4);
+        TEST_ASSERT(bb1.get_max()==pt3);
+      }
+
+      // test 2D
+      {
+        bounding_box_type2 bb1, bb2;
+        point_type2 pt1, pt2, pt3, pt4, pt5;
+        bool changed;
+
+        pt1 << 1, 2;
+        pt2 << 3, 4;
+        pt3 << 4, 1;
+        pt4 << -1, 5;
+        pt5 << 0, 3;
+
+        bb1.add(pt1);
+        bb1.add(pt2);
+
+        bb2.add(pt3);
+        bb2.add(pt4);
+
+        changed=bb1.add(bb2);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb1.get_min().x()==pt4.x());
+        TEST_ASSERT(bb1.get_min().y()==pt3.y());
+        TEST_ASSERT(bb1.get_max().x()==pt3.x());
+        TEST_ASSERT(bb1.get_max().y()==pt4.y());
+      }
+
+      // test 3D
+      {
+        bounding_box_type3 bb1, bb2;
+        point_type3 pt1, pt2, pt3, pt4, pt5;
+        bool changed;
+
+        pt1 << 1, 2, 1;
+        pt2 << 3, 4, 2;
+        pt3 << 4, 1, -1;
+        pt4 << -1, 5, 4;
+        pt5 << 0, 3, 0;
+
+        bb1.add(pt1);
+        bb1.add(pt2);
+
+        bb2.add(pt3);
+        bb2.add(pt4);
+
+        changed=bb1.add(bb2);
+        TEST_ASSERT(changed);
+        TEST_ASSERT(bb1.get_min().x()==pt4.x());
+        TEST_ASSERT(bb1.get_min().y()==pt3.y());
+        TEST_ASSERT(bb1.get_min().z()==pt3.z());
+        TEST_ASSERT(bb1.get_max().x()==pt3.x());
+        TEST_ASSERT(bb1.get_max().y()==pt4.y());
+        TEST_ASSERT(bb1.get_max().z()==pt4.z());
+      }
+    }
+
+    void inside_test()
+    {
+      // test 1D
+      {
+        bounding_box_type1 bb;
+        point_type1 pt;
+
+        pt << -1;
+        bb.set_min(pt);
+        TEST_ASSERT(bb.get_min()==pt);
+        TEST_ASSERT(bb.get_max()==pt);
+
+        pt << 1;
+        bb.set_max(pt);
+        TEST_ASSERT(bb.get_max()==pt);
+
+        // test case where there is intersection
+        pt << static_cast<data_type>(0.9);
+        TEST_ASSERT(bb.inside(pt));
+
+        // test case where there is no intersection
+        pt << static_cast<data_type>(1.1);
+        TEST_ASSERT(!bb.inside(pt));
+
+        // test case where there is contact intersection
+        pt << static_cast<data_type>(1);
+        TEST_ASSERT(bb.inside(pt));
+      }
+
+      // test 2D
+      {
+        bounding_box_type2 bb;
+        point_type2 pt;
+
+        pt << -1, -1;
+        bb.set_min(pt);
+        TEST_ASSERT(bb.get_min()==pt);
+        TEST_ASSERT(bb.get_max()==pt);
+
+        pt << 1, 1;
+        bb.set_max(pt);
+        TEST_ASSERT(bb.get_max()==pt);
+
+        // test case where there is intersection
+        pt << static_cast<data_type>(0.9), static_cast<data_type>(0.9);
+        TEST_ASSERT(bb.inside(pt));
+
+        // test case where there is no intersection
+        pt << static_cast<data_type>(1.1), static_cast<data_type>(1.1);
+        TEST_ASSERT(!bb.inside(pt));
+
+        // test case where there is contact intersection
+        pt << static_cast<data_type>(1), static_cast<data_type>(0);
+        TEST_ASSERT(bb.inside(pt));
+      }
+
+      // test 3D
+      {
+        bounding_box_type3 bb;
+        point_type3 pt;
+
+        pt << -1, -1, -1;
+        bb.set_min(pt);
+        TEST_ASSERT(bb.get_min()==pt);
+        TEST_ASSERT(bb.get_max()==pt);
+
+        pt << 1, 1, 1;
+        bb.set_max(pt);
+        TEST_ASSERT(bb.get_max()==pt);
+
+        // test case where there is intersection
+        pt << static_cast<data_type>(0.9), static_cast<data_type>(0.9), static_cast<data_type>(0.9);
+        TEST_ASSERT(bb.inside(pt));
+
+        // test case where there is no intersection
+        pt << static_cast<data_type>(1.1), static_cast<data_type>(1.1), static_cast<data_type>(1.1);
+        TEST_ASSERT(!bb.inside(pt));
+
+        // test case where there is contact intersection
+        pt << static_cast<data_type>(1), static_cast<data_type>(0), static_cast<data_type>(0);
+        TEST_ASSERT(bb.inside(pt));
+      }
+    }
+
+    void intersect_test()
+    {
+      // test 1D
+      {
+        bounding_box_type1 bb1, bb2;
+        point_type1 pt;
+
+        pt << -1;
+        bb1.set_min(pt);
+        pt << 1;
+        bb1.set_max(pt);
+
+        // test case where simple intersection
+        pt << static_cast<data_type>(0);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(2);
+        bb2.set_max(pt);
+        TEST_ASSERT(bb1.intersect(bb2));
+
+        // test case where no intersection
+        pt << static_cast<data_type>(-3);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(-2);
+        bb2.set_max(pt);
+        TEST_ASSERT(!bb1.intersect(bb2));
+
+        // test case where contact intersection
+        pt << static_cast<data_type>(1);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(2);
+        bb2.set_max(pt);
+        TEST_ASSERT(bb1.intersect(bb2));
+
+        // test case where one contains other
+        pt << static_cast<data_type>(0);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(0.5);
+        bb2.set_max(pt);
+        TEST_ASSERT(bb1.intersect(bb2));
+
+        // test case where one is contained by other
+        pt << static_cast<data_type>(-2);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(2);
+        bb2.set_max(pt);
+        TEST_ASSERT(bb1.intersect(bb2));
+      }
+
+      // test 2D
+      {
+        bounding_box_type2 bb1, bb2;
+        point_type2 pt;
+
+        pt << -1, -1;
+        bb1.set_min(pt);
+        pt << 1, 1;
+        bb1.set_max(pt);
+
+        // test case where simple intersection
+        pt << static_cast<data_type>(0), static_cast<data_type>(0);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(2), static_cast<data_type>(2);
+        bb2.set_max(pt);
+        TEST_ASSERT(bb1.intersect(bb2));
+
+        // test case where no intersection
+        pt << static_cast<data_type>(-3), static_cast<data_type>(-4);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(-2), static_cast<data_type>(0);
+        bb2.set_max(pt);
+        TEST_ASSERT(!bb1.intersect(bb2));
+
+        // test case where contact intersection
+        pt << static_cast<data_type>(1), static_cast<data_type>(0);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(2), static_cast<data_type>(2);
+        bb2.set_max(pt);
+        TEST_ASSERT(bb1.intersect(bb2));
+
+        // test case where one contains other
+        pt << static_cast<data_type>(0), static_cast<data_type>(0);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(0.5), static_cast<data_type>(0.5);
+        bb2.set_max(pt);
+        TEST_ASSERT(bb1.intersect(bb2));
+
+        // test case where one is contained by other
+        pt << static_cast<data_type>(-2), static_cast<data_type>(-2);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(2), static_cast<data_type>(2);
+        bb2.set_max(pt);
+        TEST_ASSERT(bb1.intersect(bb2));
+      }
+
+      // test 3D
+      {
+        bounding_box_type3 bb1, bb2;
+        point_type3 pt;
+
+        pt << -1, -1, -1;
+        bb1.set_min(pt);
+        pt << 1, 1, 1;
+        bb1.set_max(pt);
+
+        // test case where simple intersection
+        pt << static_cast<data_type>(0), static_cast<data_type>(0), static_cast<data_type>(0);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(2), static_cast<data_type>(2), static_cast<data_type>(2);
+        bb2.set_max(pt);
+        TEST_ASSERT(bb1.intersect(bb2));
+
+        // test case where no intersection
+        pt << static_cast<data_type>(-3), static_cast<data_type>(-4), static_cast<data_type>(-3);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(-2), static_cast<data_type>(0), static_cast<data_type>(0);
+        bb2.set_max(pt);
+        TEST_ASSERT(!bb1.intersect(bb2));
+
+        // test case where contact intersection
+        pt << static_cast<data_type>(1), static_cast<data_type>(0), static_cast<data_type>(0);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(2), static_cast<data_type>(2), static_cast<data_type>(2);
+        bb2.set_max(pt);
+        TEST_ASSERT(bb1.intersect(bb2));
+
+        // test case where one contains other
+        pt << static_cast<data_type>(0), static_cast<data_type>(0), static_cast<data_type>(0);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(0.5), static_cast<data_type>(0.5), static_cast<data_type>(0.5);
+        bb2.set_max(pt);
+        TEST_ASSERT(bb1.intersect(bb2));
+
+        // test case where one is contained by other
+        pt << static_cast<data_type>(-2), static_cast<data_type>(-2), static_cast<data_type>(-2);
+        bb2.set_min(pt);
+        pt << static_cast<data_type>(2), static_cast<data_type>(2), static_cast<data_type>(2);
+        bb2.set_max(pt);
+        TEST_ASSERT(bb1.intersect(bb2));
+      }
+    }
+};
+
+#endif
+
