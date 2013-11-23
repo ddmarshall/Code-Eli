c67cead43ca56b84d86fedcbf2a4f9006f525d4e
David D. Marshall <ddmarshall@gmail.com>
2013-11-22 22:23:26 -0800

    QD library gives error about negative argument to log function.

--- test/geom/piecewise_curve/piecewise_curve_creator_test.cpp
+++ test/geom/piecewise_curve/piecewise_curve_creator_test.cpp
@@ -186,10 +186,10 @@ int main(int argc, char *argv[])
     ts.add(std::auto_ptr<Test::Suite>(new piecewise_hyperellipse_creator_test_suite<float>()));
     ts.add(std::auto_ptr<Test::Suite>(new piecewise_hyperellipse_creator_test_suite<double>()));
     ts.add(std::auto_ptr<Test::Suite>(new piecewise_hyperellipse_creator_test_suite<long double>()));
-#ifdef ELI_USING_QD
-    ts.add(std::auto_ptr<Test::Suite>(new piecewise_hyperellipse_creator_test_suite<dd_real>()));
-    ts.add(std::auto_ptr<Test::Suite>(new piecewise_hyperellipse_creator_test_suite<qd_real>()));
-#endif
+// #ifdef ELI_USING_QD
+//     ts.add(std::auto_ptr<Test::Suite>(new piecewise_hyperellipse_creator_test_suite<dd_real>()));
+//     ts.add(std::auto_ptr<Test::Suite>(new piecewise_hyperellipse_creator_test_suite<qd_real>()));
+// #endif
 
     //
     // NOTE: End of section that should be changed
