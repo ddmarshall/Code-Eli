44f09f4c0bb38d823233551501d89c8354cd3f9c
David D. Marshall <ddmarshall@gmail.com>
2014-02-24 16:22:28 -0800

    Adding initial code framework for 4-series airfoil creator

---
+++ include/eli/geom/curve/piecewise_four_series_creator.hpp
@@ -0,0 +1,111 @@
+/*********************************************************************************
+* Copyright (c) 2013 David D. Marshall <ddmarsha@calpoly.edu>
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    David D. Marshall - initial code and implementation
+********************************************************************************/
+
+#ifndef eli_geom_curve_piecewise_four_series_creator_hpp
+#define eli_geom_curve_piecewise_four_series_creator_hpp
+
+#include <vector>
+
+#include "Eigen/Eigen"
+
+#include "eli/geom/curve/piecewise_creator_base.hpp"
+#include "eli/geom/curve/piecewise.hpp"
+#include "eli/geom/curve/bezier.hpp"
+
+namespace eli
+{
+  namespace geom
+  {
+    namespace curve
+    {
+      template<typename data__, unsigned short dim__, typename tol__>
+      class piecewise_four_series_creator : public piecewise_creator_base<data__, dim__, tol__>
+      {
+        public:
+          typedef piecewise_creator_base<data__, dim__, tol__> base_class_type;
+          typedef typename base_class_type::data_type data_type;
+          typedef typename base_class_type::point_type point_type;
+          typedef typename base_class_type::index_type index_type;
+          typedef typename base_class_type::tolerance_type tolerance_type;
+
+          piecewise_four_series_creator() : piecewise_creator_base<data_type, dim__, tolerance_type>(2, 0), corner(3) {}
+          piecewise_four_series_creator(const index_type &ns) : piecewise_creator_base<data_type, dim__, tolerance_type>(ns, 0), corner(ns+1) {}
+          piecewise_four_series_creator(const piecewise_four_series_creator<data_type, dim__, tolerance_type> &ppc)
+            : piecewise_creator_base<data_type, dim__, tolerance_type>(ppc), corner(ppc.corner) {}
+          ~piecewise_four_series_creator() {}
+
+          void set_corner(const point_type &c, const index_type &i)
+          {
+            if ((i>=0) && (i<static_cast<index_type>(corner.size())))
+              corner[i]=c;
+            else
+              assert(false);
+          }
+          point_type get_corner(const index_type &i) const
+          {
+            if ((i<0) || (i>=static_cast<index_type>(corner.size())))
+            {
+              return corner[0];
+              assert(false);
+            }
+            return corner[i];
+          }
+
+          virtual bool create(piecewise<bezier, data_type, dim__, tolerance_type> &pc) const
+          {
+            typedef piecewise<bezier, data_type, dim__, tolerance_type> piecewise_curve_type;
+            typedef typename piecewise_curve_type::curve_type curve_type;
+            typedef typename piecewise_curve_type::error_code error_code;
+
+            pc.clear();
+
+            curve_type c(1);
+            error_code err;
+            index_type nsegs(this->get_number_segments());
+
+            // do sanity check
+            if (corner.size()!=static_cast<size_t>(nsegs+1))
+            {
+              assert(false);
+              return false;
+            }
+
+            // set the start parameter
+            pc.set_t0(this->get_t0());
+
+            // set the first n edges
+            for (index_type i=0; i<nsegs; ++i)
+            {
+              c.set_control_point(corner[i], 0);
+              c.set_control_point(corner[i+1], 1);
+              err=pc.push_back(c, this->get_segment_dt(i));
+              if (err!=piecewise_curve_type::NO_ERRORS)
+              {
+                pc.clear();
+                pc.set_t0(0);
+                return false;
+              }
+            }
+
+            return true;
+          }
+
+        private:
+          void number_segments_changed() {corner.resize(this->get_number_segments()+1);}
+
+        private:
+          std::vector<point_type, Eigen::aligned_allocator<point_type>> corner;
+      };
+    }
+  }
+}
+#endif
---
+++ test/geom/piecewise_curve/piecewise_four_series_creator_test_suite.hpp
@@ -0,0 +1,251 @@
+/*********************************************************************************
+* Copyright (c) 2013 David D. Marshall <ddmarsha@calpoly.edu>
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    David D. Marshall - initial code and implementation
+********************************************************************************/
+
+#ifndef piecewise_four_series_creator_test_suite_hpp
+#define piecewise_four_series_creator_test_suite_hpp
+
+#include "eli/code_eli.hpp"
+
+#include "eli/constants/math.hpp"
+#include "eli/geom/curve/piecewise.hpp"
+#include "eli/geom/curve/piecewise_four_series_creator.hpp"
+
+#include <cmath>    // std::pow, std::exp
+#include <cassert>  // assert()
+
+#include <typeinfo> // typeid
+#include <string>   // std::string
+#include <sstream>  // std::stringstream
+#include <iomanip>  // std::setw
+#include <limits>   // std::numeric_limits
+
+template<typename data__>
+class piecewise_four_series_creator_test_suite : public Test::Suite
+{
+  private:
+    typedef eli::geom::curve::piecewise<eli::geom::curve::bezier, data__, 3> piecewise_curve_type;
+    typedef typename piecewise_curve_type::curve_type curve_type;
+    typedef typename piecewise_curve_type::point_type point_type;
+    typedef typename piecewise_curve_type::data_type data_type;
+    typedef typename piecewise_curve_type::index_type index_type;
+    typedef typename piecewise_curve_type::tolerance_type tolerance_type;
+    typedef eli::geom::curve::piecewise_four_series_creator<data__, 3, tolerance_type> point_creator_type;
+
+    tolerance_type tol;
+
+  protected:
+    void AddTests(const float &)
+    {
+      // add the tests
+      TEST_ADD(piecewise_four_series_creator_test_suite<float>::create_point_test);
+    }
+    void AddTests(const double &)
+    {
+      // add the tests
+      TEST_ADD(piecewise_four_series_creator_test_suite<double>::create_point_test);
+    }
+    void AddTests(const long double &)
+    {
+      // add the tests
+      TEST_ADD(piecewise_four_series_creator_test_suite<long double>::create_point_test);
+    }
+
+  public:
+    piecewise_four_series_creator_test_suite() : tol()
+    {
+      AddTests(data__());
+    }
+    ~piecewise_four_series_creator_test_suite()
+    {
+    }
+
+  private:
+    void octave_print(int figno, const piecewise_curve_type &pc) const
+    {
+      index_type i, pp, ns;
+      data_type tmin, tmax;
+
+      ns=pc.number_segments();
+      pc.get_parameter_min(tmin);
+      pc.get_parameter_max(tmax);
+
+      std::cout << "figure(" << figno << ");" << std::endl;
+
+      // get control points and print
+      std::cout << "cp_x=[";
+      for (pp=0; pp<ns; ++pp)
+      {
+        curve_type bez;
+        pc.get(bez, pp);
+        for (i=0; i<=bez.degree(); ++i)
+        {
+          std::cout << bez.get_control_point(i).x();
+          if (i<bez.degree())
+            std::cout << ", ";
+          else if (pp<ns-1)
+            std::cout << "; ";
+        }
+        std::cout << std::endl;
+      }
+      std::cout << "];" << std::endl;
+
+      std::cout << "cp_y=[";
+      for (pp=0; pp<ns; ++pp)
+      {
+        curve_type bez;
+        pc.get(bez, pp);
+        for (i=0; i<=bez.degree(); ++i)
+        {
+          std::cout << bez.get_control_point(i).y();
+          if (i<bez.degree())
+            std::cout << ", ";
+          else if (pp<ns-1)
+            std::cout << "; ";
+        }
+        std::cout << std::endl;
+      }
+      std::cout << "];" << std::endl;
+
+      std::cout << "cp_z=[";
+      for (pp=0; pp<ns; ++pp)
+      {
+        curve_type bez;
+        pc.get(bez, pp);
+        for (i=0; i<=bez.degree(); ++i)
+        {
+          std::cout << bez.get_control_point(i).z();
+          if (i<bez.degree())
+            std::cout << ", ";
+          else if (pp<ns-1)
+            std::cout << "; ";
+        }
+        std::cout << std::endl;
+      }
+      std::cout << "];" << std::endl;
+
+      // initialize the t parameters
+      std::vector<data__> t(129);
+      for (i=0; i<static_cast<index_type>(t.size()); ++i)
+      {
+        t[i]=tmin+(tmax-tmin)*static_cast<data__>(i)/(t.size()-1);
+      }
+
+      // set the surface points
+      std::cout << "surf_x=[";
+      for (i=0; i<static_cast<index_type>(t.size()); ++i)
+      {
+        std::cout << pc.f(t[i]).x();
+        if (i<static_cast<index_type>(t.size()-1))
+          std::cout << ", ";
+      }
+      std::cout << "];" << std::endl;
+
+      std::cout << "surf_y=[";
+      for (i=0; i<static_cast<index_type>(t.size()); ++i)
+      {
+        std::cout << pc.f(t[i]).y();
+        if (i<static_cast<index_type>(t.size()-1))
+          std::cout << ", ";
+      }
+      std::cout << "];" << std::endl;
+
+      std::cout << "surf_z=[";
+      for (i=0; i<static_cast<index_type>(t.size()); ++i)
+      {
+        std::cout << pc.f(t[i]).z();
+        if (i<static_cast<index_type>(t.size()-1))
+          std::cout << ", ";
+      }
+      std::cout << "];" << std::endl;
+
+      std::cout << "setenv('GNUTERM', 'x11');" << std::endl;
+      std::cout << "plot3(surf_x, surf_y, surf_z, '-k');" << std::endl;
+      std::cout << "hold on;" << std::endl;
+      std::cout << "plot3(cp_x', cp_y', cp_z', '-ok', 'MarkerFaceColor', [0 0 0]);" << std::endl;
+      std::cout << "hold off;" << std::endl;
+    }
+
+    void create_point_test()
+    {
+#if 0
+      // create point with specified parameterization
+      {
+        piecewise_curve_type pc;
+        point_creator_type pt_creator(4);
+        data_type dt0(3), dt1(2), dt2(3), dt3(2), t0(-1), dt;
+        point_type p0, ptemp;
+
+        // set the point
+        p0 << 1, 2, 3;
+        pt_creator.set_point(p0);
+
+        // set the times
+        pt_creator.set_t0(t0);
+        pt_creator.set_segment_dt(dt0, 0);
+        pt_creator.set_segment_dt(dt1, 1);
+        pt_creator.set_segment_dt(dt2, 2);
+        pt_creator.set_segment_dt(dt3, 3);
+
+        // test point setting
+        ptemp=pt_creator.get_point();
+        TEST_ASSERT(p0==ptemp);
+
+        // test time step settings
+        TEST_ASSERT(pt_creator.get_t0()==t0);
+        dt=pt_creator.get_segment_dt(0);
+        TEST_ASSERT(dt==dt0);
+        dt=pt_creator.get_segment_dt(1);
+        TEST_ASSERT(dt==dt1);
+        dt=pt_creator.get_segment_dt(2);
+        TEST_ASSERT(dt==dt2);
+        dt=pt_creator.get_segment_dt(3);
+        TEST_ASSERT(dt==dt3);
+
+        // create the polygon
+        TEST_ASSERT(pt_creator.create(pc));
+      }
+
+      // create point with default parameterization
+      {
+        piecewise_curve_type pc;
+        point_creator_type pt_creator(4);
+        data_type dt;
+        point_type p0, ptemp;
+
+        // set the corners
+        p0 << 1, 0, 0;
+        pt_creator.set_point(p0);
+
+        // test corner settings
+        ptemp=pt_creator.get_point();
+        TEST_ASSERT(p0==ptemp);
+
+        // test time step settings
+        TEST_ASSERT(pt_creator.get_t0()==0);
+        dt=pt_creator.get_segment_dt(0);
+        TEST_ASSERT(dt==1);
+        dt=pt_creator.get_segment_dt(1);
+        TEST_ASSERT(dt==1);
+        dt=pt_creator.get_segment_dt(2);
+        TEST_ASSERT(dt==1);
+        dt=pt_creator.get_segment_dt(3);
+        TEST_ASSERT(dt==1);
+
+        // create the polygon
+        TEST_ASSERT(pt_creator.create(pc));
+      }
+#endif
+    }
+};
+
+#endif
+
