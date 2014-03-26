2672130b728ee99b194299ea0fdef55156f5b96f
David D. Marshall <ddmarshall@gmail.com>
2014-03-26 15:55:42 -0700

    Setup basic creator class components so can test construction algorithms.

--- include/eli/geom/curve/piecewise_general_creator.hpp
+++ include/eli/geom/curve/piecewise_general_creator.hpp
@@ -13,11 +13,9 @@
 #ifndef eli_geom_curve_piecewise_general_creator_hpp
 #define eli_geom_curve_piecewise_general_creator_hpp
 
-#include <iterator>
+#include <vector>
 
-#include "eli/constants/math.hpp"
-
-#include "eli/geom/point/distance.hpp"
+#include "eli/geom/general/continuity.hpp"
 
 #include "eli/geom/curve/piecewise_creator_base.hpp"
 #include "eli/geom/curve/piecewise.hpp"
@@ -40,17 +38,96 @@ namespace eli
           typedef typename base_class_type::index_type index_type;
           typedef typename base_class_type::tolerance_type tolerance_type;
 
+          enum joint_conditions {NOT_SET=0,
+                                 VALUE_SET=1/*,
+                                 DIRECTION_SET=2*/};
+
+          enum joint_continuity {C0=general::C0,
+                                 C1=general::C1,
+                                 C2=general::C2};
+
+          class joint_data
+          {
+            public:
+              void set_f(const point_type &p) {f=p;}
+              point_type get_f() const {return f;}
+
+              void unset_fp() {use_fp=NOT_SET;}
+              void set_fp(const point_type &p)
+              {
+                use_fp=VALUE_SET;
+                fp=p;
+              }
+              joint_conditions fp_state() const {return use_fp;}
+              point_type get_fp() const {return fp;}
+
+              void unset_fpp() {use_fpp=NOT_SET;}
+              void set_fpp(const point_type &p)
+              {
+                use_fpp=VALUE_SET;
+                fpp=p;
+              }
+              joint_conditions fpp_state() const {return use_fpp;}
+              point_type get_fpp() const {return fpp;}
+
+            private:
+              point_type f;
+              joint_conditions use_fp;
+              point_type fp;
+              joint_conditions use_fpp;
+              point_type fpp;
+          };
+
         public:
-          piecewise_general_creator() : piecewise_creator_base<data_type, dim__, tolerance_type>(4, 0)
+          piecewise_general_creator()
+            : piecewise_creator_base<data_type, dim__, tolerance_type>(1, 0), joints(2),
+              joint_cont(1), max_degree(1), closed(false)
           {
           }
           piecewise_general_creator(const index_type &ns)
-            : piecewise_creator_base<data_type, dim__, tolerance_type>(ns, 0)
+            : piecewise_creator_base<data_type, dim__, tolerance_type>(ns, 0), joints(ns+1),
+              joint_cont(ns), max_degree(ns), closed_cont(C0), closed(false)
           {
           }
-          piecewise_general_creator(const piecewise_ellipse_creator_base<data_type, dim__, tolerance_type> &pcc)
-            : piecewise_creator_base<data_type, dim__, tolerance_type>(pcc) {}
-          virtual ~piecewise_general_creator() {};
+          piecewise_general_creator(const piecewise_general_creator<data_type, dim__, tolerance_type> &pcc)
+            : piecewise_creator_base<data_type, dim__, tolerance_type>(pcc), joints(pcc.joints),
+              joint_cont(pcc.joint_cont), max_degree(pcc.max_degree()), closed(pcc.closed)
+          {
+          }
+          virtual ~piecewise_general_creator()
+          {
+          };
+
+          void set_closed_continuity(const joint_continuity &cnt)
+          {
+            closed=true;
+            closed_cont=cnt;
+          }
+          void set_open() {closed=false;}
+          bool is_closed() const {return closed;}
+          bool is_open() const {return !closed;}
+
+
+          bool set_conditions(const std::vector<joint_data> &jnts, const std::vector<joint_continuity> &cnt,
+                              const std::vector<index_type> &maxd, bool cl=false)
+          {
+            index_type nsegs(static_cast<index_type>(maxd.size()));
+
+            // ensure input vectors are correct size
+            if (jnts.size()!=(nsegs+1))
+              return false;
+            if (nsegs!=(cnt.size()+1))
+              return false;
+
+            // reset the number of segments
+            set_number_segments(nsegs);
+
+            joints=jnts;
+            joint_cont=cnt;
+            closed=cl;
+
+            return true;
+          }
 
           virtual bool create(piecewise<bezier, data_type, dim__, tolerance_type> &pc) const
           {
@@ -64,6 +141,13 @@ namespace eli
 
             return false;
           }
+
+        private:
+          std::vector<joint_data> joints;
+          std::vector<joint_continuity> joint_cont;
+          std::vector<index_type> max_degree;
+          joint_continuity closed_cont;
+          bool closed;
       };
     }
   }
