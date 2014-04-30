f923e1d74ecaeedb2a8dfd9a8c909e3f03fe37c1
David D. Marshall <ddmarshall@gmail.com>
2014-04-29 23:55:12 -0700

    Added compiler flag for developer studio builds to avoid macro min/max replacement.

--- cmake/ConfigureCompiler.cmake
+++ cmake/ConfigureCompiler.cmake
@@ -106,7 +106,7 @@ if(NOT CONFIGURE_COMPILER_INCLUDED)
   # set MS Visual 2008  & 2010 Compiler flags
   if (MSVC90 OR MSVC10)
     set(CONFIGURE_COMPILER_SET_C_COMPILER_FLAGS ON)
-    set(CMAKE_C_FLAGS "/DWIN32 /D_WINDOWS /W3 /Zm1000 /Qc99"
+    set(CMAKE_C_FLAGS "/DNOMINMAX /DWIN32 /D_WINDOWS /W3 /Zm1000 /Qc99"
                       CACHE STRING "Flags used by the compiler during all build types." FORCE)
     set(CMAKE_C_FLAGS_DEBUG "/D_DEBUG /MDd /Zi  /Ob0 /Od /RTC1 /DDEBUG"
                       CACHE STRING "Flags used by the compiler during debug builds." FORCE)
@@ -118,7 +118,7 @@ if(NOT CONFIGURE_COMPILER_INCLUDED)
                       CACHE STRING "Flags used by the compiler during release minsize builds." FORCE)
 
     set(CONFIGURE_COMPILER_SET_CXX_COMPILER_FLAGS ON)
-    set(CMAKE_CXX_FLAGS "/DWIN32 /D_WINDOWS /D_CRT_SECURE_NO_WARNINGS /W3 /Zm1000 /EHsc /GR /bigobj"
+    set(CMAKE_CXX_FLAGS "/DNOMINMAX /DWIN32 /D_WINDOWS /D_CRT_SECURE_NO_WARNINGS /W3 /Zm1000 /EHsc /GR /bigobj"
                         CACHE STRING "Flags used by the compiler during all build types." FORCE)
     set(CMAKE_CXX_FLAGS_DEBUG "/D_DEBUG /MDd /Zi /Ob0 /Od /RTC1 /DDEBUG"
                         CACHE STRING "Flags used by the compiler during debug builds." FORCE)
