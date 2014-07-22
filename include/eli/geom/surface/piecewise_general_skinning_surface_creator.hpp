1d6708cac38764bcf674eca3f95743d79fc42537
David D. Marshall <ddmarshall@gmail.com>
2014-07-22 15:14:29 -0700

    Added capability to enforce maximum degree for skinning.

--- include/eli/geom/surface/piecewise_general_skinning_surface_creator.hpp
+++ include/eli/geom/surface/piecewise_general_skinning_surface_creator.hpp
@@ -586,12 +586,6 @@ namespace eli
             // FIX: Should be able to handle closed surfaces
             assert(!closed);
 
-            // FIX: Need way of handling maximum u-degree specifi0]);ation for each surface patch
-            for (i=0; i<(nribs-1); ++i)
-            {
-              assert(max_degree[i]==0);
-            }
-
             // FIX: Need to be able to handle v-direction discontinuous fu and fuu specifications
 
             // reset the incoming piecewise surface
